8076f568a2662f2422bcf7d752a457e63ed003d1
Marcin Kluczek <64478068+Marcin-Kluczek@users.noreply.github.com>
2023-10-15 20:07:22 +0200

    further improvments

--- processing_ALT.R
+++ processing_ALT.R
@@ -6,29 +6,25 @@ library(ncdf4)
 
 setwd("")
 
-#sciezka na output
-export_path <- file.path(paste0(getwd(), "/OUTPUT//"))
-
-#lista plikow w katalogu z rozszerzeniem nc
+# get list of files
 temp <- list.files(pattern="*.nc$", full.names = TRUE)
 for(i in 1:length(temp)){cat(paste0(i, "\t", temp[i]), "\n")}
 
-#otwarcie pierwszego pliku dla testu
+# check first file
 j <- 1
 nc <- ncdf4::nc_open(temp[j])
 
-#zmienne skrotowe w pliku
+# check variables of nc file
 variables <- names(nc[['var']])
 
-#atrybuty pliku nc
+# attributes of nc file
 attributes(nc)$names
-#skrotowe nazwy zmiennych
+# shortnames of nc file variables
 attributes(nc$var)$names
 
 print(paste("The file has",nc$nvars,"variables,",
             nc$ndims,"dimensions and",
             nc$natts,"NetCDF attributes"))
-
 
 #print variables and explanation
 for (i in 1:length(attributes(nc$var)$names)){
@@ -37,6 +33,7 @@ for (i in 1:length(attributes(nc$var)$names)){
              ncatt_get(nc, attributes(nc$var)$names[i])$units, "\n"))
 }
 
+#function to retrieve NetCDF time
 getNcTime <- function(nc) {
   require(lubridate)
   ncdims <- names(nc$dim) #get netcdf dimensions
@@ -71,70 +68,34 @@ getNcTime <- function(nc) {
   suppressWarnings(if (is.na(f)) stop("Could not understand the time unit format"))
   timestart + f(times)
 }
+
+# get time of file and get start and end
 time <- getNcTime(nc)
 min(time); max(time)
 
 
 
-for (i in 1:length(attributes(nc$var)$names)){
-  i <- 1
-  variable <- as.character(attributes(nc$var)$names[i])
-  name <- ncatt_get(nc, attributes(nc$var)$names[i])$long_name
-  name <- gsub(" ", "-",  name , fixed = TRUE)
-  file <- raster::brick(temp[j], var = variable)
+for (j in 1:length(temp)){
+  nc <- ncdf4::nc_open(temp[j])
   
+  # get second variable Active layer thickness
+  variable <- as.character(attributes(nc$var)$names[2])
+  
+  # get time
+  time <- getNcTime(nc)
+  name <- as.character(format(as.Date(min(time)), "%Y-%m-%d"))
+  
+  # create file name
+  name <- paste0(variable, "-", name)
+  
+  # read as raster
+  file <- raster::brick(temp[j], var = variable)
+  nc_close(nc)
+
+  # write as raster GeoTiff
   writeRaster(file, filename = name,
               format="GTiff", datatype='FLT4S', overwrite=TRUE)
 }
-
-
-for (j in 1:length(temp)){
-  
-  nc <- ncdf4::nc_open(temp[j])
-  variable <- as.character(attributes(nc$var)$names[2])
-  
-  time <- getNcTime(nc)
-  name <- as.character(format(as.Date(min(time)), "%Y-%m-%d"))
-  name <- paste0(variable, "-", name)
-  
-  file <- raster::brick(temp[j], var = variable)
-  nc_close(nc)
-  # 
-  # file[file < 0.005] <- 0
-  # file[file >= 0.005] <- 1
-  # 
-  file <- raster::calc(file, fun = mean)
-  file <- file * 100
-  plot(file)
-  writeRaster(file, filename = paste0("D:/JECAM_SM_2018_2022/OUTPUT/", name),
-              format="GTiff", datatype='INT1U', overwrite=TRUE)
-}
-
-setwd(gsub("\\\\",  "/",  readClipboard())); cat("Working directory: ", getwd())
-list_files <- list.files(pattern = "*.tif$")
-
-poligony <- rgdal::readOGR("D:/JECAM_SM_2018_2022/OUTPUT/fishnet_25.shp")
-file <- raster::brick(list_files[1])
-poligony <- spTransform(poligony, file@crs)
-
-dane <- list()
-for (i in 1:length(list_files)){
-  
-  file <- raster::brick(list_files[i])
-  data <- raster::extract(file, poligony, small = TRUE, fun = mean, na.rm = TRUE, df = TRUE)[,2]
-  name <- substring(list_files[i], 7, 16)
-  dane[[i]] <- c(data, name)
-  
-}
-
-df <- as.data.frame(t(data.frame(dane)))
-rownames(df) <- NULL
-colnames(df) <- c("swvl1", "date")
-df$swvl1 <- as.numeric(df$swvl1)
-df$date <- as.Date(df$date)
-plot(df$swvl1, type = "l")
-write.csv2(df, file = "swvl2.csv", row.names = FALSE)
-
 
 
 # Raster preprocessing ----------------------------------------------------
@@ -162,7 +123,7 @@ writeRaster(stacked_image, file = "STACK_PERMAFROST_1997_2019.tif",
 library(raster)
 setwd('D:/permafrost-change/')
 
-image <- raster::brick('PERMAFROST_ARCTIC.tif')
+image <- raster::brick('STACK_PERMAFROST_1997_2019.tif')
 
 outlist <- list() 
 nbands <- image@file@nbands
