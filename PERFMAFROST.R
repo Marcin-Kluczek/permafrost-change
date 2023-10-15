1a30183dd14a36bdae91663f42ea9560c6d31256
Marcin Kluczek <64478068+Marcin-Kluczek@users.noreply.github.com>
2023-10-15 11:19:18 +0200

    new

--- PERFMAFROST.R
+++ PERFMAFROST.R
@@ -16,8 +16,18 @@ df <- as.data.frame(df)
 df$year <- c(1997:2019)
 colnames(df) <- c("ALT", "year")
 
-loessMod10 <- loess(ALT ~ year, data=df, span=0.20) # 10% smoothing span
+df$ALT <- df$ALT * -1
+loessMod10 <- loess(ALT ~ year, data=df, span=0.75) # 10% smoothing span
 smoothed10 <- predict(loessMod10) 
 
-plot(df$ALT, type = "l")
-plot(smoothed10, type = "l")
+plot(df$ALT, type = "l", col='blue')
+lines(smoothed10, type = "l",col='red', lwd = 3)
+lines(smoothed10, type = "l",col='green', lwd = 3)
+
+
+library(zoo)
+df <- read.csv('Perfmafost_Arctic_ATL.csv')
+plot(data$ALT, type = "l", main='Simple Moving Average (SMA)',ylab='')
+lines(rollmean(data$ALT,5), col='blue')
+lines(rollmean(data$ALT,2),col='red')
+
